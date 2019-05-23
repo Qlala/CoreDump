386c951e95e758d7504624ad15ea43d9d128aa09
Qlala <rafale_feu@msn.com>
2019-05-23 20:39:38 +0200

    Interface dédié implémenter TODO : idem

---
+++ CoreDumper/CoreDumpCS/CoreDumpCS/CoreDumpInterface.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+//interface dédié à alstom
+using MyCoders;
+using CoreDumper;
+
+namespace Dumper
+{
+    class dump_reader: IDisposable
+    {
+        CoreDumpDeltaOpener<Deflate_Decoder> opener;
+        public dump_reader(string filename)
+        {
+            opener = new CoreDumpDeltaOpener<Deflate_Decoder>(new Deflate_Decoder());
+            opener.readOnlyOpen(filename);
+        }
+        public long FirstCycleRank()
+        {
+            return opener.RetrieveFirstFrame();
+        }
+        public long LastCycleRank()
+        {
+            return opener.RetrieveFrameCount();
+        }
+        public Stream GetCycle(long cycleRank)
+        {
+            return opener.RetriveFrame(cycleRank);
+        }
+        public byte[] GetBytesAtCycleAtAddress(long cycleRank, long address, long byteCount)
+        {
+            return opener.randomAccesFrame(cycleRank, address, byteCount);
+        }
+
+        void IDisposable.Dispose()
+        {
+            opener.Dispose();
+        }
+
+
+    }
+}
